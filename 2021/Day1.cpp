65ca58e17d9979bdaebd6ddbc3c4eb56b81d0883
nshipochka <nshipochka@gmail.com>
2021-12-07 10:51:15 +0200

    Complete Day 1 challenges

--- 2021/Day1.cpp
+++ 2021/Day1.cpp
@@ -15,7 +15,49 @@ int processInput() {
 	return ctr;
 }
 
+//Part 2
+int processInputWindows() {
+	int curr = 0;
+	int arr[3];
+
+	int lastSum = 0;
+	int currSum = 0;
+
+	int ctr = 0;
+
+	//first three elements
+	for (int i = 0; i < 3; ++i) {
+		if (std::cin >> curr) {
+			arr[i] = curr;
+			lastSum += curr;
+		}
+	}
+
+	char c = 'A';
+	//rest of the elements
+	while (std::cin >> curr) {
+
+		for (int i = 0; i < 2; ++i)
+			arr[i] = arr[i + 1];
+
+		arr[2] = curr;
+		currSum = 0;
+		std::cout << c++ << ":" << lastSum << "\n";
+
+		for (int i = 0; i < 3; ++i)
+			currSum += arr[i];
+
+		if (currSum > lastSum)
+			ctr++;
+
+		
+		lastSum = currSum;
+	}
+
+	return ctr;
+}
+
 int main() {
-	std::cout << processInput();
+	std::cout << processInputWindows();
 	return 0;
 }
